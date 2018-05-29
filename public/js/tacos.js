d2f696c9e26b71c8926d7ba982d5654fee557194
OlivierLeViet <orsn.tran@free.fr>
2018-05-29 12:55:58 +0200

    eeeee
    ezerzer

---
+++ public/js/tacos.js
@@ -0,0 +1,13 @@
+function edit() {
+
+    const x = document.getElementById("ee");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    // document.getElementById('aaa').innerHTML = 'block';
+
+    // const dd = document.getElementById('aaa');
+    // dd.innerHTML = 'dfgdfg';
+}
